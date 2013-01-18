1135aaf1b4c2a981ef3cf8aabf4904a528b29f8a
Hugo Mercier <hugo.mercier@oslandia.com>
2013-01-18 14:57:29 +0100

    Fix postgis.sql

--- postgis-svn/postgis/postgis.sql.in.c
+++ postgis-svn/postgis/postgis.sql.in.c
@@ -5170,7 +5170,7 @@ CREATE FUNCTION sfcgal.ST_RefGeomFromText(text)
 
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_MakeSolid(ref_geometry)
-	RETURNS geometry
+	RETURNS ref_geometry
 	AS 'MODULE_PATHNAME','sfcgal_ref_make_solid'
 	LANGUAGE 'c' IMMUTABLE STRICT;
 
@@ -5187,37 +5187,37 @@ CREATE OR REPLACE FUNCTION sfcgal.ST_3DIntersects(geom1 ref_geometry, geom2 ref_
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_Intersection(geom1 ref_geometry, geom2 ref_geometry)
-	RETURNS geometry
+	RETURNS ref_geometry
 	AS 'MODULE_PATHNAME','sfcgal_ref_intersection'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_3DIntersection(geom1 ref_geometry, geom2 ref_geometry)
-	RETURNS geometry
+	RETURNS ref_geometry
 	AS 'MODULE_PATHNAME','sfcgal_ref_intersection3D'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_ConvexHull(ref_geometry)
-	RETURNS geometry
+	RETURNS ref_geometry
 	AS 'MODULE_PATHNAME','sfcgal_ref_convexhull'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_3DConvexHull(ref_geometry)
-	RETURNS geometry
+	RETURNS ref_geometry
 	AS 'MODULE_PATHNAME','sfcgal_ref_convexhull3D'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_DelaunayTriangles(ref_geometry)
-	RETURNS geometry
+	RETURNS ref_geometry
 	AS 'MODULE_PATHNAME','sfcgal_ref_triangulate2D'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_3DDelaunayTriangles(ref_geometry)
-	RETURNS geometry
+	RETURNS ref_geometry
 	AS 'MODULE_PATHNAME','sfcgal_ref_triangulate'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
@@ -5238,12 +5238,12 @@ CREATE OR REPLACE FUNCTION sfcgal.ST_HasPlane(ref_geometry)
 	LANGUAGE 'c' IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_Extrude(ref_geometry, float8, float8, float8)
-	RETURNS geometry
+	RETURNS ref_geometry
 	AS 'MODULE_PATHNAME','sfcgal_ref_extrude'
 	LANGUAGE 'c' IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_ForceZUp(ref_geometry)
-	RETURNS geometry
+	RETURNS ref_geometry
 	AS 'MODULE_PATHNAME','sfcgal_ref_force_z_up'
 	LANGUAGE 'c' IMMUTABLE STRICT;
 
@@ -5291,7 +5291,7 @@ CREATE FUNCTION sfcgal.ST_ExactGeomFromText(text)
     LANGUAGE C IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_MakeSolid(exact_geometry)
-	RETURNS geometry
+	RETURNS exact_geometry
 	AS 'MODULE_PATHNAME','sfcgal_exact_make_solid'
 	LANGUAGE 'c' IMMUTABLE STRICT;
 
@@ -5308,37 +5308,37 @@ CREATE OR REPLACE FUNCTION sfcgal.ST_3DIntersects(geom1 exact_geometry, geom2 ex
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_Intersection(geom1 exact_geometry, geom2 exact_geometry)
-	RETURNS geometry
+	RETURNS exact_geometry
 	AS 'MODULE_PATHNAME','sfcgal_exact_intersection'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_3DIntersection(geom1 exact_geometry, geom2 exact_geometry)
-	RETURNS geometry
+	RETURNS exact_geometry
 	AS 'MODULE_PATHNAME','sfcgal_exact_intersection3D'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_ConvexHull(exact_geometry)
-	RETURNS geometry
+	RETURNS exact_geometry
 	AS 'MODULE_PATHNAME','sfcgal_exact_convexhull'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_3DConvexHull(exact_geometry)
-	RETURNS geometry
+	RETURNS exact_geometry
 	AS 'MODULE_PATHNAME','sfcgal_exact_convexhull3D'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_DelaunayTriangles(exact_geometry)
-	RETURNS geometry
+	RETURNS exact_geometry
 	AS 'MODULE_PATHNAME','sfcgal_exact_triangulate2D'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_3DDelaunayTriangles(exact_geometry)
-	RETURNS geometry
+	RETURNS exact_geometry
 	AS 'MODULE_PATHNAME','sfcgal_exact_triangulate'
 	LANGUAGE 'c' IMMUTABLE STRICT
 	COST 100;
@@ -5359,12 +5359,12 @@ CREATE OR REPLACE FUNCTION sfcgal.ST_HasPlane(exact_geometry)
 	LANGUAGE 'c' IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_Extrude(exact_geometry, float8, float8, float8)
-	RETURNS geometry
+	RETURNS exact_geometry
 	AS 'MODULE_PATHNAME','sfcgal_exact_extrude'
 	LANGUAGE 'c' IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION sfcgal.ST_ForceZUp(exact_geometry)
-	RETURNS geometry
+	RETURNS exact_geometry
 	AS 'MODULE_PATHNAME','sfcgal_exact_force_z_up'
 	LANGUAGE 'c' IMMUTABLE STRICT;
 
