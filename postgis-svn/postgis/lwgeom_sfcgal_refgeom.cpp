99fe3eb3a9930addba586a49171a5801ac25f24c
Hugo Mercier <hugo.mercier@oslandia.com>
2013-01-23 16:37:34 +0100

    Fix the way referenced geometries are handled

--- postgis-svn/postgis/lwgeom_sfcgal_refgeom.cpp
+++ postgis-svn/postgis/lwgeom_sfcgal_refgeom.cpp
@@ -25,6 +25,7 @@ extern "C" {
 #include "postgres.h"
 #include "fmgr.h"
 #include "utils/memutils.h"
+#include "nodes/execnodes.h"
 
 #include "../postgis_config.h"
 #include "lwgeom_pg.h"
@@ -39,99 +40,215 @@ extern "C" {
 class GeometryPool
 {
 public:
-	static void reference( void* context, SFCGAL::PreparedGeometry* geometry )
+	static void init( MemoryContext context )
 	{
-		pool_[context].push_back( geometry );
 	}
 
-	static void deleteAll( void* context )
+	static bool isEmpty( MemoryContext context )
 	{
-		if ( pool_.find( context ) == pool_.end() ) {
-			return;
+		/* mandatory implementation */
+		return false;
+	}
+
+	static void check( MemoryContext context )
+	{
+	}
+
+	static void stats( MemoryContext context, int level )
+	{
+		fprintf(stderr, "%s: Ref geometry context\n", context->name);
+	}
+
+	/**
+	 * Is the given Geometry still existing ?
+	 */
+	static bool isReferenced( SFCGAL::PreparedGeometry* geometry )
+	{
+		// this has to be fast, the geometry is then the key of the pool_ map
+		return pool_.find( geometry ) != pool_.end();
+	}
+
+	/**
+	 * Save a reference to the given Geometry.
+	 * It is associated with a MemoryContext with a proper life cycle.
+	 */
+	static void reference( FunctionCallInfo fcinfo, SFCGAL::PreparedGeometry* geometry )
+	{
+		//
+		// Find the memory context used to store SFCGAL::Geometry*
+		// Ideally, this would be in the closest parent context of the current function evaluation.
+		//
+		// If the current function call is not part of an ExprNode, it means this context is volatile and will be
+		// reset between calls within the same tuple access. In this case, attach our context to the MessageContext
+		MemoryContext parentContext;
+		
+		if ( fcinfo->flinfo->fn_expr != NULL ) {
+			parentContext = CurrentMemoryContext;
 		}
-		GeometryList& l = pool_[ context ];
-		if ( l.size() == 0 ) {
-			return;
+		else {
+			parentContext = MessageContext;
 		}
-		for ( int i = l.size() - 1; i >= 0; --i ) {
-			SFCGAL::PreparedGeometry* g = l[i];
-			delete g;
-			l.pop_back();
+		
+		MemoryContext childContext = 0;
+		// If a child context already exists, use it
+		ChildContextMap::const_iterator child_it = childContext_.find( parentContext );
+		if ( child_it != childContext_.end() ) {
+			childContext = child_it->second;
+		}
+		else {
+			// Else, create a new one.
+			// We won't allocate anything in the new context. It is only used for its destruction callbacks.
+			// It is thus allocated with the minimum required size
+			char contextName[] = "SFCGAL";
+			childContext = MemoryContextCreate( T_AllocSetContext, sizeof(MemoryContextData) + strlen(contextName) + 1,
+							    &GeometryPool::contextMethods,
+							    parentContext,
+							    contextName );
+
+			// associate this new context to its parent
+			childContext_[ parentContext ] = childContext;
+		}
+		
+		// force next reset. When the parent will be reset, reset will be called on this child
+		childContext->isReset = false;
+		
+		// now reference the geometry
+		pool_[ geometry ] = childContext;
+	}
+	
+	static void deleteContext( MemoryContext context )
+	{
+		for ( GeometryReference::iterator it = pool_.begin(); it != pool_.end(); ++it ) {
+			if ( it->second == context ) {
+				delete it->first;
+				pool_.erase( it );
+			}
+		}
+
+		// delete the ChildContextMap part
+		for ( ChildContextMap::iterator it = childContext_.begin(); it != childContext_.end(); ++it ) {
+			if ( it->second == context ) {
+				childContext_.erase( it );
+				break;
+			}
 		}
 	}
+
+	static MemoryContextMethods contextMethods;
 private:
-	typedef std::vector<SFCGAL::PreparedGeometry*> GeometryList;
-	static std::map<void*, GeometryList> pool_;
+	typedef std::map<SFCGAL::PreparedGeometry*, MemoryContext> GeometryReference;
+	static GeometryReference pool_;
+
+	// memory context => child memory context
+	typedef std::map<MemoryContext, MemoryContext> ChildContextMap;
+	static ChildContextMap childContext_;
 };
-std::map<void*, GeometryPool::GeometryList> GeometryPool::pool_;
+GeometryPool::GeometryReference GeometryPool::pool_;
+GeometryPool::ChildContextMap GeometryPool::childContext_;
 
-/**
- * Memory context destructor
- */
-void (*oldDelete) (MemoryContext );
-void refDelete( MemoryContext context )
-{
-	GeometryPool::deleteAll( context );
-	(*oldDelete) ( context );
-}
-
-/**
- * Memory context destructor used by reset
- */
-void (*oldReset) (MemoryContext );
-void refReset( MemoryContext context )
-{
-	GeometryPool::deleteAll( context );
-	(*oldReset) ( context );
-}
-
-/**
- * Returns the MemoryContext where geometries will be referenced and delete afterwards
- */
-MemoryContext get_reference_context()
-{
-	//
-	// returns the memory context used to store SFCGAL::Geometry*
-	// Ideally, this would be in the closest parent context of the current function evaluation.
-	MemoryContext refContext = MessageContext;
-
-	// overwrite destructors if needed
-	if ( refContext->methods->delete_context != refDelete ) {
-		oldDelete = refContext->methods->delete_context;
-		oldReset = refContext->methods->reset;
-		refContext->methods->delete_context = refDelete;
-		refContext->methods->reset = refReset;
-	}
-	return refContext;
-}
+MemoryContextMethods GeometryPool::contextMethods = {
+	NULL, // alloc
+	NULL, // free
+	NULL, // realloc
+	GeometryPool::init,
+	GeometryPool::deleteContext, // reset
+	GeometryPool::deleteContext,
+	NULL, // get_chunk_space
+	GeometryPool::isEmpty,
+	GeometryPool::stats
+#ifdef MEMORY_CONTEXT_CHECKING
+	, GeometryPool::check
+#endif
+};
 
 /**
  * Get a Geometry pointer from function arguments
  */
-SFCGAL::PreparedGeometry* get_geometry_arg( FunctionCallInfo fcinfo, size_t n )
+static SFCGAL::PreparedGeometry* get_geometry_arg( FunctionCallInfo fcinfo, size_t n )
 {
 	void** p = (void**)PG_GETARG_POINTER( n );
 	SFCGAL::PreparedGeometry* pp = (SFCGAL::PreparedGeometry*)(*p);
+	if ( ! GeometryPool::isReferenced( pp ) ) {
+		// The geometry has been deleted
+		return 0;
+	}
 	return pp;
 }
 
+/**
+ * Get a Geometry pointer from function arguments, returns error if the geometry does not exist anymore
+ */
+static SFCGAL::PreparedGeometry* get_geometry_arg_secure( FunctionCallInfo fcinfo, size_t n )
+{
+	void** p = (void**)PG_GETARG_POINTER( n );
+	SFCGAL::PreparedGeometry* pp = (SFCGAL::PreparedGeometry*)(*p);
+	if ( ! GeometryPool::isReferenced( pp ) ) {
+		lwerror( "Unable to access deleted geometry !" );
+		return 0;
+	}
+	return pp;
+}
 
 /**
  * Prepare a Geometry for return
  */
-Datum return_geometry( SFCGAL::PreparedGeometry* geometry )
+Datum prepare_for_return( FunctionCallInfo fcinfo, SFCGAL::PreparedGeometry* geo )
 {
+	GeometryPool::reference( fcinfo, geo );
+
+	// allocate space for a pointer
 	void** p = (void**)palloc( sizeof(void*) );
-	*p = (void*)(geometry);
+	*p = (void *)geo;
 	return Datum(p);
 }
 
+/**
+ * Only used for debugging purpose
+ */
+static void display_children( MemoryContext context, int level, MemoryContext current )
+{
+	char indent[level*2+1];
+	memset( indent, ' ', level*2 );
+	indent[level*2] = 0;
+
+	if ( context == current && level > 0 ) {
+		indent[0] = '>';
+	}
+	
+	lwnotice( "%s%s %p", indent, context->name, context );
+
+	MemoryContext child = context->firstchild;
+	while ( child != 0 ) {
+		display_children( child, level + 2, current );
+		child = child->nextchild;
+	}
+}
+
+/**
+ * Only used for debugging purpose
+ */
+static void display_contexts( MemoryContext refContext )
+{
+	MemoryContext c = refContext;
+	while ( c != TopMemoryContext ) {
+		c = c->parent;
+	}
+
+	display_children( c, 0, refContext );
+}
+
+extern "C" {
+	PG_FUNCTION_INFO_V1(sfcgal_ref_in);
+	PG_FUNCTION_INFO_V1(sfcgal_ref_from_text);
+}
 
 /**
  * Convert a WKT to a referenced geometry
  */
-SFCGAL::PreparedGeometry* refGeometryFromCString( char* cstring )
+extern "C" Datum sfcgal_ref_in(PG_FUNCTION_ARGS)
 {
+	char* cstring = PG_GETARG_CSTRING( 0 );
+
 	std::auto_ptr<SFCGAL::PreparedGeometry> g;
 	try
 	{
@@ -140,37 +257,33 @@ SFCGAL::PreparedGeometry* refGeometryFromCString( char* cstring )
 	catch ( std::exception& e )
 	{
 		lwerror("First argument geometry could not be converted to SFCGAL: %s", e.what() );
-		return 0;
+		PG_RETURN_NULL();
 	}
 
-	MemoryContext referenceContext = get_reference_context();
-	SFCGAL::PreparedGeometry* geo = g.release();
-	GeometryPool::reference( referenceContext, geo );
-	return geo;
+	return prepare_for_return( fcinfo, g.release() );
 }
 
-extern "C" {
-	PG_FUNCTION_INFO_V1(sfcgal_ref_in);
-	PG_FUNCTION_INFO_V1(sfcgal_ref_from_text);
-}
-
-extern "C" Datum sfcgal_ref_in(PG_FUNCTION_ARGS)
-{
-	char* cstring = PG_GETARG_CSTRING( 0 );
-	SFCGAL::PreparedGeometry* geo = refGeometryFromCString( cstring );
-	if (!geo)
-		PG_RETURN_NULL();
-	return return_geometry( geo );
-}
-
+/**
+ * Convert a WKT to a referenced geometry
+ */
 extern "C" Datum sfcgal_ref_from_text(PG_FUNCTION_ARGS)
 {
+	//	display_contexts( CurrentMemoryContext );
 	text *wkttext = PG_GETARG_TEXT_P(0);
 	char *cstring = text2cstring(wkttext);
-	SFCGAL::PreparedGeometry* geo = refGeometryFromCString( cstring );
-	if (!geo)
+
+	std::auto_ptr<SFCGAL::PreparedGeometry> g;
+	try
+	{
+		g = SFCGAL::io::readEwkt( cstring, strlen(cstring) );
+	}
+	catch ( std::exception& e )
+	{
+		lwerror("First argument geometry could not be converted to SFCGAL: %s", e.what() );
 		PG_RETURN_NULL();
-	return return_geometry( geo );
+	}
+
+	return prepare_for_return( fcinfo, g.release() );
 }
 
 /**
@@ -184,6 +297,14 @@ extern "C" {
 extern "C" Datum sfcgal_ref_out(PG_FUNCTION_ARGS)
 {
 	SFCGAL::PreparedGeometry* g = get_geometry_arg( fcinfo, 0 );
+	if ( !g ) {
+		char deleted[] = "-deleted-";
+		char* retstr = (char*)palloc( strlen(deleted) + 1 );
+		strncpy( retstr, deleted, strlen(deleted) + 1 );
+
+		lwnotice( "Referenced geometries must not be stored" );
+		PG_RETURN_CSTRING( retstr );
+	}
 
 	std::string wkt = g->asEWKT( /* exact */ -1 );
 	char * retstr = (char*)palloc( wkt.size() + 1 );
@@ -202,23 +323,20 @@ extern "C" {
 
 extern "C" Datum sfcgal_ref_from_geom(PG_FUNCTION_ARGS)
 {
-    GSERIALIZED *geom1;
-    
-    geom1 = (GSERIALIZED *)PG_DETOAST_DATUM(PG_GETARG_DATUM(0));
-
-    std::auto_ptr<SFCGAL::PreparedGeometry> g1;
-    try {
-	g1 = POSTGIS2SFCGALp( geom1 );
-    }
-    catch ( std::exception& e ) {
-	lwerror("First argument geometry could not be converted to SFCGAL: %s", e.what() );
-	PG_RETURN_NULL();
-    }
-
-    MemoryContext referenceContext = get_reference_context();
-    SFCGAL::PreparedGeometry* geo = g1.release();
-    GeometryPool::reference( referenceContext, geo );
-    return return_geometry( geo );
+	GSERIALIZED *geom1;
+	
+	geom1 = (GSERIALIZED *)PG_DETOAST_DATUM(PG_GETARG_DATUM(0));
+	
+	std::auto_ptr<SFCGAL::PreparedGeometry> g1;
+	try {
+		g1 = POSTGIS2SFCGALp( geom1 );
+	}
+	catch ( std::exception& e ) {
+		lwerror("First argument geometry could not be converted to SFCGAL: %s", e.what() );
+		PG_RETURN_NULL();
+	}
+	
+	return prepare_for_return( fcinfo, g1.release() );
 }
 
 
@@ -232,7 +350,10 @@ extern "C" {
 
 extern "C" Datum sfcgal_geom_from_ref(PG_FUNCTION_ARGS)
 {
-	SFCGAL::PreparedGeometry* g = get_geometry_arg( fcinfo, 0 );
+	SFCGAL::PreparedGeometry* g = get_geometry_arg_secure( fcinfo, 0 );
+	if ( !g ) {
+		PG_RETURN_NULL();
+	}
 	GSERIALIZED* result = SFCGAL2POSTGIS( *g, false );
 	PG_RETURN_POINTER( result );
 }
@@ -244,7 +365,10 @@ extern "C" Datum sfcgal_geom_from_ref(PG_FUNCTION_ARGS)
  */
 #define WRAPPER_TYPE_refGeometry 1
 #define WRAPPER_INPUT_refGeometry( i ) \
-	SFCGAL::PreparedGeometry* BOOST_PP_CAT( input, i ) = get_geometry_arg( fcinfo, i );
+	SFCGAL::PreparedGeometry* BOOST_PP_CAT( input, i ) = get_geometry_arg_secure( fcinfo, i ); \
+	if ( ! BOOST_PP_CAT( input, i ) ) { \
+		PG_RETURN_NULL();	    \
+	}
 
 #define WRAPPER_ACCESS_INPUT_refGeometry( i )  \
 	BOOST_PP_CAT( input, i )->geometry()
@@ -255,8 +379,8 @@ extern "C" Datum sfcgal_geom_from_ref(PG_FUNCTION_ARGS)
 #define WRAPPER_CONVERT_RESULT_refGeometry()   /* */
 #define WRAPPER_RETURN_refGeometry() \
 	SFCGAL::PreparedGeometry* geo = new SFCGAL::PreparedGeometry( result, input0->SRID() ); \
-	GeometryPool::reference( get_reference_context(), geo ); \
-	return return_geometry( geo );
+	return prepare_for_return( fcinfo, geo );
+
 #define WRAPPER_TO_CSTR_refGeometry( i )   			\
 	"%s", BOOST_PP_CAT( input, i ) ->asEWKT().c_str()
 
