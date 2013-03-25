22f8149ef23661626cb3dce97230bf3233a40bf5
Hugo Mercier <hugo.mercier@oslandia.com>
2013-03-25 14:42:33 -0400

    Remove postgis sources (See Oslandia/postgis on github)

--- postgis-svn/extensions/upgradeable_versions.mk
+++
@@ -1,4 +0,0 @@
-UPGRADEABLE_VERSIONS = \
-	2.0.0 \
-    2.0.1 \
-    2.0.2
--- postgis-svn/raster/test/regress/check_gdal.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-	CASE
-		WHEN strpos(postgis_gdal_version(), 'GDAL_DATA') <> 0
-			THEN false
-		ELSE NULL
-	END;
